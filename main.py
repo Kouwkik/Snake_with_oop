75bffd7c11ce5718e6a27c1e933c7940dcf604b9
kouwkik <vanya.kharchenko.2018@mail.ru>
2025-06-22 20:50:11 +0300

    second commit

--- main.py
+++ main.py
@@ -1,41 +1,148 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.math import Vector2
 
 pygame.init()
 
+title_font = pygame.font.Font(None, 60)
+score_font = pygame.font.Font(None, 40)
+
 GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
 
-cell_size = 30 # высота игровой сетки
-number_of_cells = 25 # ширина игровой сетки
+cell_size = 25 # высота игровой сетки
+number_of_cells = 20 # ширина игровой сетки
+
+OFFSET = 75
 
 class Food:
-    def __init__(self):
-        self.position = Vector2(5, 6) # стартовая позиция еды
+    def __init__(self, snake_body):
+        self.position = self.generate_random_pos(snake_body) # стартовая позиция еды
 
 
     def draw(self):
-        food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
-        pygame.draw.rect(screen, DARK_GREEN, food_rect) # создание поля для еды (квадрата)
+        food_rect = pygame.Rect(OFFSET + self.position.x * cell_size, OFFSET + self.position.y * cell_size, cell_size, cell_size)
+        screen.blit(food_surface, food_rect) # отображение еды на экране
 
-screen = pygame.display.set_mode((cell_size * number_of_cells, cell_size * number_of_cells)) # создание экрана с игровой сеткой
+
+    def generate_random_cell(self):
+        x = random.randint(0, number_of_cells - 1)
+        y = random.randint(0, number_of_cells - 1)
+        return Vector2(x, y)
+
+
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+        while position in snake_body:
+            position = self.generate_random_cell()
+        return position
+
+
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        self.direction = Vector2(1, 0)
+        self.add_segment = False
+
+    def draw(self):
+        for segment in self.body:
+            segment_rect = pygame.Rect(OFFSET + segment.x * cell_size, OFFSET + segment.y * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)
+
+
+    def update(self):
+        self.body.insert(0, self.body[0] + self.direction)
+        if self.add_segment == True:
+            self.add_segment = False
+        else:
+            self.body = self.body[:-1]
+
+    def reset(self):
+        self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        self.direction = Vector2(1, 0)
+
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food(self.snake.body)
+        self.state = "RUNNING"
+        self.score = 0
+
+    def draw(self):
+        self.food.draw() # создание объекта с изображением еды
+        self.snake.draw() # создание объекта с изображением змеи
+
+    def update(self):
+        if self.state == "RUNNING":
+            self.snake.update()
+            self.check_collision_with_food()
+            self.check_collision_with_edges()
+            self.check_collision_with_tail()
+
+    def check_collision_with_food(self):
+        if self.snake.body[0] == self.food.position:
+            self.food.position = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
+            self.score += 1
+
+    def check_collision_with_edges(self):
+        if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
+            self.game_over()
+        if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1:
+            self.game_over()
+
+    def game_over(self):
+        self.snake.reset()
+        self.food.position = self.food.generate_random_pos(self.snake.body)
+        self.state = "STOPPED"
+        self.score = 0
+
+    def check_collision_with_tail(self):
+        headless_body = self.snake.body[1:]
+        if self.snake.body[0] in headless_body:
+            self.game_over()
+
+screen = pygame.display.set_mode((2 * OFFSET + cell_size * number_of_cells, 2 * OFFSET + cell_size * number_of_cells)) # создание экрана с игровой сеткой
 
 pygame.display.set_caption("Snake")
 
 clock = pygame.time.Clock()
 
-food = Food() # создание объекта с изображением еды
-food_surface = pygame.image.load("apple.webp")
+game = Game()
+
+food_surface = pygame.image.load("apple-119.png")
+food_surface = pygame.transform.scale(food_surface, (cell_size, cell_size))
+
+SNAKE_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SNAKE_UPDATE, 200)
 
 while True:
     for event in pygame.event.get(): # получаем все события с момента последнего запуска и помещаем в список
-        if event .type == pygame.QUIT: # если нажали крестик - конец игры
+        if event.type == SNAKE_UPDATE:
+            game.update() # обновление координат змеи
+        if event.type == pygame.QUIT: # если нажали крестик - конец игры
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN: # управление змеей стрелками
+            if game.state == "STOPPED":
+                game.state = "RUNNING"
+            if event.key == pygame.K_UP and game.snake.direction != Vector2(0, 1):
+                game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0, -1):
+                game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1, 0):
+                game.snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1, 0):
+                game.snake.direction = Vector2(1, 0)
+
     # Рисование
     screen.fill(GREEN) # заполнение заднего фона зеленым цветом
-    food.draw() # нарисовали квадрат еды
+    pygame.draw.rect(screen, DARK_GREEN, (OFFSET-5, OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10), 5)
+    game.draw()
+    title_surface = title_font.render("Retro Snake", True, DARK_GREEN)
+    score_surface = score_font.render(str(game.score), True, DARK_GREEN)
+    screen.blit(title_surface, (OFFSET-5, 20))
+    screen.blit(score_surface, (OFFSET-5, OFFSET + cell_size*number_of_cells + 10))
 
     pygame.display.update() # очищает экран
     clock.tick(60) # цикл while и весь код внутри будут выполняться 60 раз в секунду (частота кадров)
